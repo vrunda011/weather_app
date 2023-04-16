2688cd7693109058bdd08bd6433b67dd8c9d1bd1
Vrunda Patel <vrunda@Vrundas-MacBook-Pro.local>
2023-04-16 22:13:20 +0530

    added city parameter to weather endpoint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,12 +12,17 @@ def index():
 def weather():
     url = 'http://api.weatherapi.com/v1/current.json'
     key = '4d97521ec9d3488d90281006231504'
-    q = 'vadodara'
+    q = request.args.get('city', 'vadodara')
     response = requests.get("%s?key=%s&q=%s"%(url, key, q))
     response = response.json()
-    temp = str(response['current']['temp_c'])
+
     current_time = datetime.now().strftime("%B %d, %Y")
-    return render_template("weather.html",date=current_time,city='vadodara',temp=temp)
+
+    if 'error' in response:
+        return render_template("weather.html",date=current_time,city='City not found',temp=0)
+
+    temp = str(response['current']['temp_c'])
+    return render_template("weather.html",date=current_time,city=q,temp=temp)
 
 
 if __name__ == "__main__":
