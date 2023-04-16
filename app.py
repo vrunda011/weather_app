85198d8563b6a587fd3abaac3c0c2c49b8ad813b
Vrunda Patel <vrunda@Vrundas-MacBook-Pro.local>
2023-04-16 16:59:45 +0530

    updated UI

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,11 +10,14 @@ def index():
 
 @app.route("/weather")
 def weather():
-    response = requests.get("http://api.weatherapi.com/v1/current.json?key=4d97521ec9d3488d90281006231504&q=vadodara")
+    url = 'http://api.weatherapi.com/v1/current.json'
+    key = '4d97521ec9d3488d90281006231504'
+    q = 'vadodara'
+    response = requests.get("%s?key=%s&q=%s"%(url, key, q))
     response = response.json()
-    return "<div style='color:red'>Weather of vadodara : "+str(response['current']['temp_c']) + "</div>"
-    
-    
+    temp = str(response['current']['temp_c'])
+    current_time = datetime.now().strftime("%B %d, %Y")
+    return render_template("weather.html",date=current_time,city='vadodara',temp=temp)
 
 
 if __name__ == "__main__":
